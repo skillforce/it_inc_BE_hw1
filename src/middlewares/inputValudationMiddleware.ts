2e0d9708f73c7be9380f54aa21056c4e85e9ce8c
denis_tatarinov <skillforce@mail.ru>
2024-12-29 09:48:00 +0300

    middleware adjustments

--- src/middlewares/inputValudationMiddleware.ts
+++ src/middlewares/inputValudationMiddleware.ts
@@ -13,11 +13,11 @@ interface ValidateUnexpectedFieldRequest extends Request {
 export const inputValidationMiddleware = (req:Request, res:Response, next:NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map(error => ({
+        const errorsMessages = errors.array().map(error => ({
                 message:error.msg,
                 field:error.type==='field' ? error.path : ''
         }));
-        res.status(400).json({errorMessages});
+        res.status(400).json({errorsMessages});
     }else{
         next()
     }
