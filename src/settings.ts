f3bb537ecee6c42d580a9e16622bde7a6c1c2a10
denis_tatarinov <skillforce@mail.ru>
2024-12-29 09:35:30 +0300

    add vercel.json

--- src/settings.ts
+++ src/settings.ts
@@ -4,7 +4,7 @@ config(); // добавление переменных из файла .env в p
 export const SETTINGS = {
     PORT: process.env.PORT || 3003,
       PATH: {
-        VIDEOS: '/api/videos',
-        TESTING: '/api/testing',
+        VIDEOS: '/videos',
+        TESTING: '/testing',
     },
 }
